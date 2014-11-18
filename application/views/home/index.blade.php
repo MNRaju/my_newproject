6cb0566e05a2eb2b207b367b04645d0312912cea
MNRaju <nookarajumalladi@gmail.com>
2014-11-18 20:58:30 +0530

    this example

---
+++ application/views/home/index.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    {{ Asset::container('bootstrapper')->styles(); }}
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="../assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">Project name</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="navbar-form pull-right">
+              <input class="span2" type="text" placeholder="Email">
+              <input class="span2" type="password" placeholder="Password">
+              <button type="submit" class="btn">Sign in</button>
+            </form>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <div id="hero-content">
+	        <h1>Hello, Laravel world!</h1>
+	        <h2 class="text-warning">This is an example page of calling AJAX in Laravel using Bootsraper bundle and <a href="https://github.com/eldarion/bootstrap-ajax">Bootstrap AJAX by Github user Eldarion</a> , very simple way to implement ajax calls in your application</h2>
+        </div>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+      	<div class="span4">
+          <h2>Refresh data after submission</h2>
+          <p>Pull in content when some action is done (for example new vote count after submitting a vote)</p>
+
+          <p class="votecount lead" data-refresh-url="votes">12 Votes</p>
+          
+          <p>
+          	<a href="vote" class="btn ajax votebtn" data-method="post" data-refresh=".votecount" data-replace=".votebtn">
+			  	<i class="icon icon-heart"></i> Submit Vote &raquo;
+			</a>
+		  </p>
+        </div>
+        <div class="span4">
+          	<h2>Replace content DEMO</h2>
+          	<p>Click the button to replace the content in the hero unit with AJAX loaded content </p>
+			<a href="replace" class="btn ajax" data-method="post" data-replace="#hero-content">
+		  		<i class="icon icon-refresh"></i> Replace Content in HERO unit &raquo;
+			</a>
+        </div>
+        <div class="span4">
+          <h2>Append content DEMO</h2>
+            <p>Click the button to append the content in the hero unit with AJAX loaded content </p>
+          	<p>
+				<a href="replace" class="btn ajax" data-method="post" data-append="#hero-content">
+			  		<i class="icon icon-plus"></i> Append Content to HERO unit &raquo;
+				</a>
+			</p>
+       	</div>
+      </div>
+
+      <hr>
+
+      <div class="row">
+        <div class="span4">
+	        <h2>AJAX Form Example</h2>
+			<form class="form ajax" action="submit" data-replace="#submit-status" method="post">
+			{{ Form::label('name', 'Name');}}
+			{{ Form::text('name', null, array('class' => 'span3', 'placeholder' => 'Type your name...'));}}
+			{{ Form::block_help('Example block-level help text here.');}}
+			{{ Form::labelled_checkbox('checker', 'Check me out');}}
+			{{ Form::submit('Submit');}}
+			</form>
+			<p id="submit-status"></p>
+		</div>
+        <div class="span4">
+          <h2>Redirect DEMO</h2>
+            <p>Click the button to redirect your user instead via AJAX. The route "redirect" sends over a variable called "location" with the location where to redirect the user</p>
+          	<p>
+				<a href="redirect" class="btn ajax" data-method="post">
+			  		<i class="icon icon-share"></i> Redirect the user &raquo;
+				</a>
+			</p>
+       	</div>
+       	<div class="span4">
+          <h2>Content Fragments Update</h2>
+            <p>Click the button to change fragments on your page at once (notice the Votes count, hero unit changes)
+              <a href="fragments" class="btn ajax" data-method="post">
+	        <i class="icon icon-refresh"></i> Update fragments &raquo;
+	      </a>
+            </p>
+          	{{ HTML::image('img/maks.jpg', 'author', array('class' => 'thumbnail')); }}
+       	</div>
+
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>Made by <a href="http://maxoffsky.com"> Maks</a> 2012, open source.</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    {{ Asset::container('bootstrapper')->scripts(); }}
+    {{ HTML::script('js/bootstrap-ajax.js');}}
+
+  </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,22 @@
+# AJAX examples using [Laravel](http://laravel.com) and [Bootstrap AJAX](https://github.com/eldarion/bootstrap-ajax)
+
+## See live demo at [Pagodabox deployed code](http://laravelajaxexample.gopagoda.com/)
+This is a demo of simplifying AJAX calls withing Laravel application using Bootstrapper Bundle and Bootstrap-ajax extension 
+
+The source code features are :
++ replace content via AJAX calls
++ append content via AJAX calls
++ Form submittal and displaying result via AJAX calls
++ Loading data after successfull AJAX call 
+
+To get this working on your local machine just copy this folder into your htdocs MAMP directory or www WAMP directory and navigate to /public folder of this application.
+
+There is no Database setup here but you could do that in a few minutes with Laravel.
+
+## Contributing to This code
+
+Contributions are encouraged and welcome. Submit pull requests or ask questions if something's not clear
+
+## License
+
+This source is under MIT license.
